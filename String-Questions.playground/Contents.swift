f5084abb39dd53cb5b2b391e0d22a7e4f4413985
robihamanto <rrobie44@gmail.com>
2020-08-24 17:09:00 +0700

    Palindrome with two solutions completed

--- String-Questions.playground/Contents.swift
+++ String-Questions.playground/Contents.swift
@@ -1,3 +1,36 @@
+import Foundation
 import UIKit
 
-var str = "Hello, playground"
+
+/// Compelxity: O(1)
+func isPalindrome(text: String) -> Bool {
+    
+    let reversedText = String(text).reversed()
+    
+    if text != String(reversedText) {
+        return false
+    }
+    
+    return true
+}
+
+isPalindrome(text: "kasurrusak")
+
+
+/// Complexity: O(n)
+func isPalindrome2(text: String) -> Bool {
+    
+    var reversedText = ""
+
+    for item in text {
+        reversedText = reversedText + String(item)
+    }
+    
+    if reversedText != text {
+        return false
+    }
+    
+    return true
+}
+
+isPalindrome2(text: "ANJAY")
